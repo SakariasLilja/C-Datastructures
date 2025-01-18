9e391fc03e9a2bf26401b1c88ef2a5d8157287cc
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-01-18 23:49:22 +0200

    Added main, and supporting files

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "slds.h"
+
+int main() {
+    out("Hello World!\n");
+
+    return 0;
+}
---
+++ src/slds.c
@@ -0,0 +1,7 @@
+#include "slds.h"
+
+#include <stdio.h>
+
+void out(const char* str) {
+    printf("%s", str);
+}
