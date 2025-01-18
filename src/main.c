8ecc34d3dd262618f1c8db3bbdbcd1cbc10f6415
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-01-19 01:05:36 +0200

    Changed contents of slds

--- src/main.c
+++ src/main.c
@@ -3,7 +3,23 @@
 #include "slds.h"
 
 int main() {
-    out("Hello World!\n");
+    List *list = list_init();
 
+    int values[] = {4, 2};
+
+    for (int i = 0; i < 2; i++) {
+        if( list_append(list, values[i]) ) {
+            printf("Successfully added %d\n", values[i]);
+        }
+        else {
+            printf("Error\n");
+        }
+    }
+
+    printf("\n");
+
+    list_print(list);
+
+    list_free(list);
     return 0;
 }
--- src/slds.h
+++ src/slds.h
@@ -1,1 +1,45 @@
-void out(const char* str);
+typedef struct Node Node;
+typedef struct List List;
+
+struct Node {
+    Node *prev;
+    Node *next;
+    int value;
+};
+
+struct List {
+    Node *head;
+    Node *last;
+    int size;
+};
+
+/**
+ * @brief Initializes a list to the heap
+ * 
+ * @param list The list to initialize
+ * @return List* Pointer to the initialized list
+ */
+List* list_init();
+
+/**
+ * @brief Frees the list from the heap memory
+ * 
+ * @param list The list to free from the heap
+ */
+void list_free(List* list);
+
+/**
+ * @brief Appends an non-initialized element to a list
+ * 
+ * @param list The list onto which to append
+ * @param value The appended value
+ * @return int 1 if success, 0 else
+ */
+int list_append(List *list, int value);
+
+/**
+ * @brief Prints the contents of the list to the standard output
+ * 
+ * @param list The list whose contents to print
+ */
+void list_print(List *list);
