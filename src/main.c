79b7c416ab193deddc2037de67d9573c111bdcca
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-01-19 02:06:35 +0200

    Added test header for testing functionality and performance of data structures

--- src/main.c
+++ src/main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 
-#include "slds.h"
+#include "sltest.h"
 
 int main() {
-    List *list = list_init();
-
-    int values[] = {4, 2};
-
-    for (int i = 0; i < 2; i++) {
-        if( list_append(list, values[i]) ) {
-            printf("Successfully added %d\n", values[i]);
-        }
-        else {
-            printf("Error\n");
-        }
-    }
-
-    printf("\n");
-
-    list_print(list);
-
-    list_free(list);
+    linkedListPerformance();
+    
     return 0;
 }
---
+++ src/sltest.h
@@ -0,0 +1,2 @@
+void linkedListTest();
+void linkedListPerformance();
