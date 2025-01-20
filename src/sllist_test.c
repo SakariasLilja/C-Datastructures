ff6388aede2f3cc08804a9d38571533a28458022
Sakarias Lilja WSL <sakarias.lilja@gmail.com>
2025-01-20 18:55:15 +0200

    Added test for list_get method

--- src/sllist_test.c
+++ src/sllist_test.c
@@ -117,3 +117,28 @@ float* listReversePerformance() {
 
     return times;
 }
+
+float* listGetPerformance() {
+    warmup();
+    float *times = calloc(TEST_SIZE, sizeof(float));
+
+    if(times == NULL) {
+        return NULL;
+    }
+
+    printf("Measuring list get performance...\n");
+    for(int i = 0; i < TEST_SIZE; i++) {
+        List *list = list_init();
+        for(unsigned int j = 0; j < ARR[i]; j++) {
+            list_append(list, 0);
+        }
+        clock_t ticks = clock();
+        list_get(list, ARR[i] - 2);
+        ticks = clock() - ticks;
+        times[i] = (((float)ticks)/CLOCKS_PER_SEC) * 1000.0F;
+        list_free(list);
+    }
+    printf("Done!\n");
+
+    return times;
+}
